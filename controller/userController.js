ceee858dfa6cf18a0323e2aa0e69cf8cf008e02e
sobhan83sefidkar <mongodbacc10@gmail.com>
2025-03-24 11:37:25 +0330

    Update userController.js

--- controller/userController.js
+++ controller/userController.js
@@ -58,14 +58,14 @@ export const login = async (req, res) => {
             secure: true,
         }
 
-        res.status(200).cookie("auth-token", token, tokenOptions).json({ notification: { success: true, message: "با موفقیت وارد شدید" , username : user.username , admin : user.admin } })
+        res.status(200).cookie("token", token, tokenOptions).json({ notification: { success: true, message: "با موفقیت وارد شدید" , username : user.username , admin : user.admin } })
     } catch (err) {
         res.status(500).json({ notification: { success: false , message: err.message || err}})
     }
 }
 export const logout = async (req , res) => {
     try{
-        res.status(200).cookie("auth-token" , "").json({ notification: { success: true, message: "شما از حساب خود خارج شدید" } })
+        res.status(200).cookie("token" , "").json({ notification: { success: true, message: "شما از حساب خود خارج شدید" } })
     }catch(err){
         res.status(500).json({ notification: { success: false , message: err.message || err}  })
     }
