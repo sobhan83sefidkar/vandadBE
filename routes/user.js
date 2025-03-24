f83b9b7b8d737de701e7f56bca5ed08592576a31
sobhan83sefidkar <mongodbacc10@gmail.com>
2025-03-24 12:57:21 +0330

    Update user.js

--- routes/user.js
+++ routes/user.js
@@ -6,7 +6,7 @@ const router = express.Router()
 
 router.post("/register", register)
 router.post("/login",  login)
-router.get("/logout",  logout)
+router.post("/logout",  logout)
 router.get("/check-auth", checkLogin ,checkAuth)
 router.get("/get-users", getUser)
 
